f62f801a6e87f4b79c17e013bdb3697258997491
mrunaldhangare <mrunalyetekar@gmail.com>
2023-01-17 21:41:21 +0000

    completed generateInitials function

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -8,7 +8,7 @@ export function capitalize(word) {
 export function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
+  return firstName.substring(0,1) + "." + lastName.substring(0,1);
 }
 
 export function addVAT(originalPrice, vatRate) {
