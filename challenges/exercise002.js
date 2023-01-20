e8700c9b50bcc03ed1ea7d71254520f61c4843f8
mrunaldhangare <mrunalyetekar@gmail.com>
2023-01-20 12:55:15 +0000

    completed exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,36 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+
+  return person.city === "Manchester" ? true : false;
+
 }
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+
+  const busCapacity = 40;
+
+  return Math.ceil(people / busCapacity);
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+
+  const animalName = "sheep";
+  return arr.filter((element) => element === animalName).length;
 }
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+
+  if (person.address.postCode[0] === "M" && person.address.city === "Manchester") {
+    return true;
+  }
+  return false;
 }
